87f157038ef6554bb0f7891d9eb7c46f0052dd29
Mauricio Miraglio <mauricio.miraglio@gmail.com>
2019-06-25 15:41:41 -0300

    First commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useAuth0 } from "../react-auth0-wrapper";
+
+const NavBar = () => {
+    const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+    return (
+        <div>
+            {!isAuthenticated && (
+                <button onClick={() => loginWithRedirect({ redirect_uri: window.location.origin })}>
+                    Log in
+                </button>
+            )}
+            {isAuthenticated && <button onClick={() => logout()}>Log out</button>}
+            {isAuthenticated && (
+                <span>
+                    <Link to="/">Home</Link>&nbsp;
+                    <Link to="/profile">Profile</Link>
+                </span>
+            )}
+        </div>
+    );
+};
+
+export default NavBar;
